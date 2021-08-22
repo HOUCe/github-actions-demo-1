3fb7580818e9a064684dc9b1b6c89eef5d8f7e2d
houce <houce@zhihu.com>
2021-08-22 12:33:18 +0800

    feat: add xlint

---
+++ xlint.js
@@ -0,0 +1,36 @@
+'use strict'
+const { spawnSync } = require('child_process')
+
+function getDiffFiles (commit = 'HEAD', anotherCommit = 'origin/master') {
+  const shell = `git --no-pager diff ${commit} ${anotherCommit} --stat --name-only '**/*.js'`
+  const diffFiles = getListFromShell(shell)
+
+  return diffFiles
+}
+
+const diffList = getDiffFiles(
+  'HEAD',
+  'origin/master'
+)
+
+console.log('diffList ==>', diffList)
+
+if (Array.isArray(diffList) && diffList.length) {
+  const lintProcess = spawnSync(
+    'npx eslint',
+    diffList,
+    {
+      stdio: ['inherit', 'inherit', 'pipe'],
+      shell: true
+    }
+  )
+  const errMsg = lintProcess.stderr.toString()
+  if (errMsg) {
+    console.error(errMsg)
+    process.exit(1)
+  } else {
+    console.log('well done. lint pass!')
+  }
+} else {
+  console.log('本次无须 diff lint.')
+}
