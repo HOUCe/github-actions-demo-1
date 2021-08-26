e6b491db531aaf7aad1da7293dc73ba8d193f420
houce <houce@zhihu.com>
2021-08-26 16:07:24 +0800

    feat: add MSUP as welcom

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,7 +30,7 @@
 
 <script>
 export default {
-  name: 'Hello World, Hello MSUP~!',
+  name: 'Hello World!',
   props: {
     msg: String
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App, Lucas~!"/>
+    <HelloWorld msg="Welcome to Your Vue.js App, welcome to MSUP~!"/>
   </div>
 </template>
 
